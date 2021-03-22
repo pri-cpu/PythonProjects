920eecab42caacbdbcd0ec6aab237e3f5cb72326
Priya Patel <47262834+pri-yas@users.noreply.github.com>
2021-03-22 17:28:55 -0400

    Add files via upload
    Type and Comparisons project

---
+++ Comparing_Dataset_Entries.py
@@ -0,0 +1,13 @@
+id_1 = "#4"
+average_grade_1 = "A"
+test_score_1 = 90
+
+id_2 = "#5"
+average_grade_2 = "A"
+test_score_2 = 70
+
+no_duplicates = id_1 != id_2
+print("No duplicates.")
+print(no_duplicates)
+
+same_average = average_grade_1 == average_grade_2
